cc91430a8e00f02a0911b3415284a2bb22c5ce8c
cisse410 <dramisse90@gmail.com>
2024-03-17 21:46:14 +0000

    Firebase configuration

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:waref/routes/routes.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() => runApp(const HomeApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const HomeApp());
+}
 
 class HomeApp extends StatelessWidget {
   const HomeApp({super.key});
